f4a98e58d4c7afb0c52fb3f9f188a5a591d985df
Gustav Westberg <gustavwes@gmail.com>
2018-01-26 11:45:39 +0100

    Initial Setup of project

---
+++ Pizzeria/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pizzeria.Models;
+
+namespace Pizzeria.Controllers
+{
+    public class HomeController : Controller
+    {
+        private TomasosContext _context;
+
+        public HomeController(TomasosContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult RegisterCustomer()
+        {
+
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Pizzeria/Startup.cs
+++ Pizzeria/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Pizzeria.Models;
 
 namespace Pizzeria
 {
@@ -15,6 +17,10 @@ namespace Pizzeria
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            var conn = @"Server=localhost;Database=MyDB;Trusted_Connection=True;
+ ConnectRetryCount=0";
+            services.AddDbContext<TomasosContext>(options => options.UseSqlServer(conn));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,10 +30,12 @@ namespace Pizzeria
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
