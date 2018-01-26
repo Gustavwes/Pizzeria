52096820ceffc368bfc881941089723a8ec02d13
Gustav Westberg <31844570+Gustavwes@users.noreply.github.com>
2018-01-26 14:02:53 +0100

    Started Register Customer html

--- Pizzeria/Models/Kund.cs
+++ Pizzeria/Models/Kund.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Pizzeria.Models
 {
@@ -11,13 +12,21 @@ namespace Pizzeria.Models
         }
 
         public int KundId { get; set; }
+        [Required(ErrorMessage = "Fältet får inte vara tomt")]
         public string Namn { get; set; }
+        [Required(ErrorMessage = "Fältet får inte vara tomt")]
         public string Gatuadress { get; set; }
+        [Required(ErrorMessage = "Fältet får inte vara tomt")]
         public string Postnr { get; set; }
+        [Required(ErrorMessage = "Fältet får inte vara tomt")]
         public string Postort { get; set; }
+        [Required(ErrorMessage = "Fältet får inte vara tomt")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Fältet får inte vara tomt")]
         public string Telefon { get; set; }
+        [Required(ErrorMessage = "Fältet får inte vara tomt")]
         public string AnvandarNamn { get; set; }
+        [Required(ErrorMessage = "Fältet får inte vara tomt")]
         public string Losenord { get; set; }
 
         public ICollection<Bestallning> Bestallning { get; set; }
